4c99f911fcb31c007c7f09964caea7978b9c3b7e
guillegr01 <gllrmglrncn2001@gmail.com>
2024-12-18 22:34:46 +0100

    utils casi terminados

--- utils.ts
+++ utils.ts
@@ -1,50 +1,50 @@
 import { Collection } from "mongodb";
 import { User, UserModel } from "./types.ts";
-import { Comment, CommentModel } from "./types.ts";
 import { Post, PostModel } from "./types.ts";
+import { Comment, CommentModel } from "./types.ts";
 
-export const fromModelToUser = async (um:UserModel, postCollection: Collection<PostModel>, commentCollection: Collection<CommentModel>): User => {
-    const posts = await postCollection.find({ authorId: um._id }).toArray();
-    return {
-        id: um._id!.toString(),
-        name: um.name,
-        password: um.password,
-        email: um.email,
-        posts: posts.map((pm:PostModel) => {})
-    }
-};
-
-/*
-export interface User {
-    id: string;
-    name: string;
-    password: string;
-    email: string;
-    posts: string[];       // IDs de las publicaciones
-    comments: string[];    // IDs de los comentarios
-    likedPosts: string[];  // IDs de las publicaciones con "like"
-}
-
-import { Collection } from "mongodb";
-import { User, UserModel, Post } from "./types.ts";
-
-export const fromModelToUser = async (
-    um: UserModel,
-    postCollection: Collection<Post>
-): Promise<User> => {
-    const posts = await postCollection.find({ authorId: um._id }).toArray();
-    const comments = await postCollection.find({ "comments.authorId": um._id }).toArray();
-    const likedPosts = await postCollection.find({ "likes.userId": um._id }).toArray();
+export const fromModelToUser = async (um: UserModel, uc: Collection<UserModel>, pc: Collection<PostModel>, cc: Collection<CommentModel>): Promise<User> => {
+    
+    const posts = await pc.find({ author: um._id }).toArray();
+    
+    const comments = await cc.find({ author: um._id }).toArray();
 
     return {
         id: um._id!.toString(),
         name: um.name,
         password: um.password,
         email: um.email,
-        posts: posts.map(post => post._id.toString()),
-        comments: comments.map(comment => comment._id.toString()),
-        likedPosts: likedPosts.map(like => like._id.toString())
+        posts: await Promise.all(posts.map(pm => fromModelToPost(pm,uc, pc, cc))),
+        comments: await Promise.all(comments.map(cm => fromModelToComment(cm, uc, pc, cc))),
+        likedPosts: [] // Opcional: Implementar si necesario
     };
 };
 
-*/
+export const fromModelToPost = async (pm: PostModel, uc: Collection<UserModel>, pc: Collection<PostModel>, cc: Collection<CommentModel>): Promise<Post> => {
+    
+    const author = await uc.findOne({ _id: pm.author });
+    
+    const comments = await cc.find({ post: pm._id }).toArray();
+
+    return {
+        id: pm._id!.toString(),
+        content: pm.content,
+        author: author ? await fromModelToUser(author, uc, pc, cc) : undefined,
+        comments: await Promise.all(comments.map(cm => fromModelToComment(cm, uc, pc, cc))),
+        likes: [] // Opcional: Implementar si necesario
+    };
+};
+
+export const fromModelToComment = async (cm: CommentModel, uc: Collection<UserModel>, pc: Collection<PostModel>, cc: Collection<CommentModel>): Promise<Comment> => {
+    // Obtener autor del comentario
+    const author = await uc.findOne({ _id: cm.author });
+    // Obtener el post relacionado
+    const post = await pc.findOne({ _id: cm.post });
+
+    return {
+        id: cm._id!.toString(),
+        text: cm.text,
+        author: author ? await fromModelToUser(author, uc, pc, cc) : null,
+        post: post ? await fromModelToPost(post, uc, pc, cc) : null
+    };
+};
